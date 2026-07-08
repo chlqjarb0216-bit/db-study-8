4351ab1f26d5338403944d36e9d7f864e86a147e
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-07-08 15:26:51 +0900

    퀴즈 7,8

---
+++ quiz/quiz07.sql
@@ -0,0 +1,16 @@
+--1.
+--Student 테이블의 birthday 컬럼을 사용하여 생일이 1월인 학생의 이름과 birthday 를 출력하세요.
+SELECT STUDNO,
+       NAME,
+       BIRTHDAY
+FROM STUDENT
+WHERE TO_CHAR(BIRTHDAY, 'MM') = 1;
+
+
+--2.
+--emp 테이블의 hiredate 컬럼을 사용하여 입사일이 1,2,3 월인 사람들의 사번과 이름, 입사일을 출력하세요.
+SELECT EMPNO,
+       ENAME,
+       HIREDATE
+FROM EMP
+WHERE TO_CHAR(HIREDATE, 'MM') IN ( 1, 2, 3 );
